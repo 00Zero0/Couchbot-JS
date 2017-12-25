c473a5d62fd7319016545fe60650d191dfc600ca
Zero Lot <minatouzumakin001@gmail.com>
2017-12-25 12:09:59 +0545

    adding timer

--- timer.js
+++ timer.js
@@ -4,8 +4,42 @@ const commands = require('./commands');
 
 let data = [];
 
+function settimer(message)
+{
+    let contents = message.content.split(' ');
 
+    switch(contents.length())
+    {
+        case 1:
+            return displaytimeleft(message);
+        case 3:
+        {
+            if(contents[1]=='set')
+            {
+                let minutes = parseInt(contents[2]);
+                if(minutes==NaN || minutes<1){
+                    message.channel.send('Incorrect time, please type time in minutes!');
+                    return;
+                }
+                addtimer(message.author.id, minutes);
+                return message.reply('Your timer for ' + minutes + ' has been set!');
+            }
+        }
+        default:
+            message.channel.send('Syntax Error! Please type `' + commands.getPrefix() + 'timer ` to see your time left OR\n`' +
+                            commands.getPrefix() + 'timer "time-in-minutes` " to set timer');
+    }
+}
 
+function displaytimeleft(message)
+{
+
+}
+
+function addtimer(id, minutes)
+{
+    
+}
 
 module.exports = {
     load: function(){
